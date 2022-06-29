ba7ba93a9a7bfb4861ddc7a93da09d89046e39c8
Gichelle Amon <amongichelle@gmail.com>
2022-06-28 21:11:56 -0700

    add first feature board state

--- src/app/modules/board/store/reducers/board.reducer.ts
+++ src/app/modules/board/store/reducers/board.reducer.ts
@@ -1,14 +1,15 @@
 import { Action, createReducer, on } from '@ngrx/store';
 
 
-export const boardFeatureKey = 'board';
+export const boardFeatureKey = 'BOARD_STATE';
 
 export interface State {
+  tiles: string[]
 
 }
 
 export const initialState: State = {
-
+  tiles: [...Array(9).fill('*')]
 };
 
 export const reducer = createReducer(
