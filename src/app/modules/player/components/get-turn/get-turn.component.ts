061af060b0d357f84867bad5b0abb31e4ab11b34
Gichelle Amon <amongichelle@gmail.com>
2022-06-29 10:12:01 -0700

    generate new component to get turn state

---
+++ src/app/modules/player/components/get-turn/get-turn.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-get-turn',
+  templateUrl: './get-turn.component.html',
+  styleUrls: ['./get-turn.component.scss']
+})
+export class GetTurnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
