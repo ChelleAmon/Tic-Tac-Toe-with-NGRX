8f129660190c4822aa496af3ba8c2831cdb278c7
Gichelle Amon <amongichelle@gmail.com>
2022-06-29 13:33:59 -0700

    dispatch the action of the next player

--- src/app/modules/board/services/board.service.ts
+++ src/app/modules/board/services/board.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { State } from 'src/app/reducers';
+import { actionNextPlayer } from '../../player/store/actions/player.actions';
 import { getTilesSelector } from '../store/selectors/board.selectors';
 
 @Injectable({
@@ -10,7 +11,13 @@ export class BoardService {
 
   tiles$;
 
+
   constructor(private store: Store<State>) {
     this.tiles$ = this.store.select(getTilesSelector)
+
+   }
+
+   nextTurn(){
+     this.store.dispatch(actionNextPlayer())
    }
 }
