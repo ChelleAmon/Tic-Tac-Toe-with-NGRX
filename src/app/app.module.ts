3e96945f486b39cf02a17bef44426747787feeb5
Gichelle Amon <amongichelle@gmail.com>
2022-06-28 20:23:15 -0700

    generate board component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { BoardTilesComponent } from './modules/board/component/board-tiles/board-tiles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    BoardTilesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/modules/board/component/board-tiles/board-tiles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board-tiles',
+  templateUrl: './board-tiles.component.html',
+  styleUrls: ['./board-tiles.component.scss']
+})
+export class BoardTilesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
