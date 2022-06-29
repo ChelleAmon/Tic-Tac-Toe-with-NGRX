46178a6e91506db1efc69279dbef7245a2aa0f2f
Gichelle Amon <amongichelle@gmail.com>
2022-06-28 21:12:15 -0700

    add first part of board feature selector for viewing

--- src/app/modules/board/store/selectors/board.selectors.ts
+++ src/app/modules/board/store/selectors/board.selectors.ts
@@ -1,2 +1,9 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import * as fromBoard from '../reducers/board.reducer'
 
+const selectBoardFeatureState = createFeatureSelector<fromBoard.State>(fromBoard.boardFeatureKey)
+
+export const getTilesSelector = createSelector(
+  selectBoardFeatureState,
+  (state) => state.tiles
+)
