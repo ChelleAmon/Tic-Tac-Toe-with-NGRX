33cd37552801518249bb1e57fad514f6178c8ec0
Gichelle Amon <amongichelle@gmail.com>
2022-06-28 20:31:01 -0700

    generate board module

--- src/app/modules/board/board.module.ts
+++ src/app/modules/board/board.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BoardRoutingModule } from './board-routing.module';
+import { BoardComponent } from './pages/board/board.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BoardComponent
+  ],
   imports: [
     CommonModule,
     BoardRoutingModule
